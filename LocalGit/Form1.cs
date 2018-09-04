03b422f3abe0239e3b7570e3ff39443d259b8332
CHAYSE.MCMILLAN <CHAYSE.MCMILLAN@BU158-PROG20>
2018-09-04 08:14:10 -0600

    First Comit

---
+++ LocalGit/Form1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LocalGit
+{
+    public partial class LocalGit : Form
+    {
+        //constructor
+        public LocalGit()
+        {
+            InitializeComponent();
+        }
+
+        //message box button
+        private void btnHello_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello world of git.");
+        }
+    }
+}
